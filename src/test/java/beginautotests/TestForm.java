bb9e4e91ed6f68a8cbd311aeb1cee1331f35ae82
junforpeace <soapcream@gmail.com>
2022-04-05 20:48:22 +0200

    что-то новое

---
+++ src/test/java/beginautotests/TestForm.java
@@ -0,0 +1,12 @@
+package beginautotests;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+public class TestForm {
+    @Test
+    void
+}
+
